6ea1c691a624e1582ec0fd41b4d6023a12b40d1c
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-15 12:12:33 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from "./components/Layout/Layout";
 
 import Website from "./pages/Website";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Properties from "./pages/Properties/Properties";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Website />} />
+            <Route path="/properties" element={<Properties />}/>
           </Route>
         </Routes>
       </Suspense>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,51 +1,53 @@
-import React, { useState } from "react";
-import "./Header.css";
-import { BiMenuAltRight } from "react-icons/bi";
-import { getMenuStyles } from "../../utils/common";
-import useHeaderColor from "../../hooks/useHeaderColor";
-import OutsideClickHandler from "react-outside-click-handler";
+  import React, { useState } from "react";
+  import "./Header.css";
+  import { BiMenuAltRight } from "react-icons/bi";
+  import { getMenuStyles } from "../../utils/common";
+  import useHeaderColor from "../../hooks/useHeaderColor";
+  import OutsideClickHandler from "react-outside-click-handler";
+import { Link, NavLink } from "react-router-dom";
 
-const Header = () => {
-  const [menuOpened, setMenuOpened] = useState(false);
-  const headerColor = useHeaderColor();
+  const Header = () => {
+    const [menuOpened, setMenuOpened] = useState(false);
+    const headerColor = useHeaderColor();
 
-  return (
-    <section className="h-wrapper" style={{ background: headerColor }}>
-      <div className="flexCenter innerWidth paddings h-container">
-        {/* logo */}
-        <img src="./logo.png" alt="logo" width={100} />
+    return (
+      <section className="h-wrapper" style={{ background: headerColor }}>
+        <div className="flexCenter innerWidth paddings h-container">
+          {/* logo */}
+          <Link to="/">
+          <img src="./logo.png" alt="logo" width={100} />
+          </Link>
 
-        {/* menu */}
-        <OutsideClickHandler
-          onOutsideClick={() => {
-            setMenuOpened(false);
-          }}
-        >
-          <div
-            // ref={menuRef}
-            className="flexCenter h-menu"
-            style={getMenuStyles(menuOpened)}
+          {/* menu */}
+          <OutsideClickHandler
+            onOutsideClick={() => {
+              setMenuOpened(false);
+            }}
           >
-            <a href="#residencies">Residencies</a>
-            <a href="#value">Our Value</a>
-            <a href="#contact-us">Contact Us</a>
-            <a href="#get-started">Get Started</a>
-            <button className="button">
-              <a href="mailto:zainkeepscode@gmail.com">Contact</a>
-            </button>
+            <div
+              // ref={menuRef}
+              className="flexCenter h-menu"
+              style={getMenuStyles(menuOpened)}
+            >
+                <NavLink to="/properties">Properties</NavLink>
+                <a href="mailto:anshulsanjaywagh@gmail.com">Contact</a>
+                {/*Login Button*/}
+                <button className="button">
+                  Login
+                </button>
+            </div>
+          </OutsideClickHandler>
+
+          {/* for medium and small screens */}
+          <div
+            className="menu-icon"
+            onClick={() => setMenuOpened((prev) => !prev)}
+          >
+            <BiMenuAltRight size={30} />
           </div>
-        </OutsideClickHandler>
-
-        {/* for medium and small screens */}
-        <div
-          className="menu-icon"
-          onClick={() => setMenuOpened((prev) => !prev)}
-        >
-          <BiMenuAltRight size={30} />
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  };
 
-export default Header;
+  export default Header;
