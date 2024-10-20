4b21709657ba4e2f9ab435004390c21bd93c9e36
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-20 23:31:38 +0530

    app and properties update

--- src/pages/Properties/Properties.jsx
+++ src/pages/Properties/Properties.jsx
@@ -2,10 +2,32 @@ import React from 'react'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import './Properties.css'
 import useProperties from '../../hooks/useProperties'
+import { PuffLoader } from 'react-spinners'
 const Properties = () => {
 
   const {data, isError, isLoading} = useProperties()
-  console.log(data)
+  
+  if(isError){
+    return(
+      <div className='wrapper'>
+      <span> Error while fetching data </span>
+      </div>
+    )
+  }
+
+  if(isLoading){
+    return(
+      <div className="wrapper flexCenter" style={{height: "60vh"}}>
+      <PuffLoader
+      height="80"
+      width="80"
+      radius={1}
+      color="#4066ff"
+      aria-label="puff-loading"
+      />
+      </div>
+    )
+  }
   return (
     <div className="wrapper">
       <div className="flexColCenter paddings innerWidth properties-container">
