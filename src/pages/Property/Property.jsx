f2c3a48aac9e6515e5cc12fc22ce1ee938ee5a02
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-24 22:44:43 +0530

    update in app, property folder

---
+++ src/pages/Property/Property.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Property = () => {
+  return (
+    <div className="wrapper">
+        This is an individual property path
+
+    </div>
+  )
+}
+
+export default Property
