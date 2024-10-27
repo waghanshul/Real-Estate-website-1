89b54646264a7718f444096ba10abd324b0d7386
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-10-27 19:45:08 +0530

    property update

--- src/pages/Property/Property.jsx
+++ src/pages/Property/Property.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import {useQuery} from 'react-query'
-import {useLocation} from 'react-router-dom'
-import { getProperty } from '../../utils/api'
-import { PuffLoader } from 'react-spinners'
-import { AiFillHeart } from "react-icons/ai";
+import React from 'react';
+import {useQuery} from 'react-query';
+import {useLocation} from 'react-router-dom';
+import { getProperty } from '../../utils/api';
+import { PuffLoader } from 'react-spinners';
+import { AiFillHeart, AiTwotoneCar } from "react-icons/ai";
+import "./Property.css";
+
+import {FaShower} from "react-icons/fa";
+import {MdMeetingRoom} from "react-icons/md";
+
 
 const Property = () => {
-  const {pathname} = useLocation()
-  const id = pathname.split("/").slice(-1)[0]
+  const {pathname} = useLocation();
+  const id = pathname.split("/").slice(-1)[0];
   const {data, isLoading, isError} = useQuery(["resd", id], ()=> 
     getProperty(id)
 );
 
-  if(isLoading)
-  {return (
+console.log(data);
+
+  if(isLoading) {return (
     <div className="wrapper">
       <div className="flexCenter paddings">
         <PuffLoader/>
@@ -43,6 +49,50 @@ const Property = () => {
 
         {/* image */}
         <img src={data?.image} alt="home image"/>
+
+
+        <div className="flexCenter property-details">
+
+
+          {/* left */}
+          <div className="flexColCenter left">
+
+
+            {/* head */}
+            <div className= "flexStart head">
+              <span className="primaryText">{data?.title}</span>
+              <span className="orangeText" style={{fontSize: '1.5rem'}}>$ {data?.price}</span>
+            </div>
+
+            {/* facilities */}
+            <div className="flexStart facilities">
+
+              {/* bathrooms */}
+              <div className="flexStart facility">
+                <FaShower size={20} color="#1F3E72" />
+                <span>{data?.facilities?.bathrooms} Bathrooms</span>
+              </div>
+
+                {/* parkings */}
+              <div className="flexStart facility">
+                <AiTwotoneCar size={20} color="#1F3E72"/>
+                <span>{data?.facilities.parkings} Parking</span>
+              </div>
+
+                   {/* rooms */}
+              <div className="flexStart facility">
+                <MdMeetingRoom size={20} color="#1F3E72"/>
+                <span>{data?.facilities.bedrooms} rooms</span>
+              </div>
+            </div>
+          </div>
+
+
+          {/* right side */}
+          <div className="right">
+            this is right side
+          </div>
+        </div>
       </div>
     </div>
   );
