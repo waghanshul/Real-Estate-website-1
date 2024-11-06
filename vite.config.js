a08d0e049a819dab5c6583607e24aa120cfe0899
Anshul Wagh <anshulsanjaywagh@gmail.com>
2024-11-06 13:33:57 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: './client',  // Set the root directory to the 'client' folder
   optimizeDeps: {
     include: ['leaflet'],
+  },
+  build: {
+    outDir: '../dist',  // Output directory outside of the 'client' folder
   },
 });
